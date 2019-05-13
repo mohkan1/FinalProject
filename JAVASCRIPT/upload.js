05543ffddc2fb3ba5a344209340dad41f34ed1bf
mohkan <maher.99875@gmail.com>
2019-05-13 13:23:23 +0200

    comment

--- JAVASCRIPT/upload.js
+++ JAVASCRIPT/upload.js
@@ -1,3 +1,6 @@
+//This function is used to increase the width of a div called mybar while downloading the file to the server. 
+//It is just used to show how much is left till everything fully uploaded
+//It increases the width with 1 px every 1 ms tills the width is 100 px
 document.getElementById("put").addEventListener('click' , function (){
 
   
